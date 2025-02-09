9503e04d7e8e005e6c373d2e895cddb105898c5b
Zaidhasan25 <Zaidhasan5433@gmail.com>
2025-02-09 04:43:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 123456
+let accountEmail = "zaidhasan223@gmail.com"
+var accountPassword = "zaidkhan123"
+accountCity = "karachi"
+let accountState;
+
+accountId = 2  // not allowed //
+
+/*
+var not use because scope issue 
+*/
+
+
+console.log(accountId); 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# js-practice-githup
-zaid-practice-chai aur code 
+
