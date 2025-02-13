36e14f0bcc1558f79b65c5b40da24e06b63b21d8
Zaidhasan25 <Zaidhasan5433@gmail.com>
2025-02-13 11:40:42 +0000

    co

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+" use strict ";   // treat all js code is newer version //
+
+
+
+console.log("code");         console.log ("zaid ") ;// this is allowed but code rediebility is high//
+
+
+let name = "zaid hasan"
+let age =    24
+let isLoggedin = false
+let state;
+
+
+
+// number => 2 to 50 
+// bigints
+// string  => ""
+// boolean => true / false 
+// null =>standalne value 
+// undefined =>
+// symbol => uniqe 
+
+
+// object 
+
+console.log(typeof "zaid");
+
+
+
+
+
+
+
+
+     
+
+
