3efc2b019ad50b98250d602d5b5cc61a84c78123
Zaidhasan25 <Zaidhasan5433@gmail.com>
2025-02-18 22:13:33 +0000

    conversion datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,41 @@
+let score = "zaid"
+
+console.log(score);
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33  yh string value ko change hwkr number bngyi //
+// "33abc" => NaN  yh Not a Number hay is mien nh huwa change  // 
+// true => 1 : fales => 0  is mien value change hwgyi. true ki value ayi 1 ; aur fales ki ayi 0 //
+
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => ture : 0 => false  yh value aghe 1 hay tw true aur 0 hay tw value false hay//
+// "" => false aghr string mien empty value hay tw false value hay //
+// "zaid" => true string mien koi value hay tw value true hay // 
+
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
